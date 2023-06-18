56e7d250b38d9a781e1c0f1a9dad6201b6780e7f
CHENYU <thischenyu@gmail.com>
2023-06-18 22:06:37 +0800

    added utils

---
+++ spring-boot-demo/src/test/java/com/heycoding/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heycoding.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
